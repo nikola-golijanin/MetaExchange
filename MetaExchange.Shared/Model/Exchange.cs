cec5f4d26c5b5b82f1193f9938a549438ac62a27
nikola-golijanin <golijanin.nikola@outlook.com>
2024-12-13 11:02:39 +0100

    Added comment

--- MetaExchange.Shared/Model/Exchange.cs
+++ MetaExchange.Shared/Model/Exchange.cs
@@ -3,6 +3,8 @@ namespace MetaExchange.Shared.Model;
 public class Exchange
 {
     public DateTime AcqTime { get; set; }
+    
+    //For the sake of simplicity ExchangeName is used as some exchange unique identifier
     public string ExchangeName { get; set; }
     public List<Bid> Bids { get; set; }
     public List<Ask> Asks { get; set; }
